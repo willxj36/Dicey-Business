311af5abfde1c6e364f7f6f6edb44847e36bec4a
willxj36 <willxj36@gmail.com>
2021-01-11 14:20:47 -0700

    finished lab including above and beyond features

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ class Die {
     constructor() {
         $('#dice-container').append(`<div class="dice border border-3 border-dark rounded-3 m-3" id="${counter}"></div>`);
         this.div = $(`#${counter}`);
-        this.roll();
-        this.value = value;
+        this.value = this.roll();
+        this.div.click(function() { //roll only die clicked on
+            dice[this.id - 1].value = dice[this.id - 1].roll();
+        })
+        this.div.dblclick(function(e) { //delete die double clicked
+            $(this).remove();
+            dice.splice(this.id - 1, 1);
+        })
     };
 
     roll() {
         let value = Math.floor(Math.random() * 6) + 1;
-        this.value = `${value}`;
-        this.div.text(this.value);
+        this.div.text(value);
         return value;
     }
 }
 
-let div;
 let value;
 let counter = 0;
 let dice = [];
@@ -22,13 +26,19 @@ let dice = [];
 $('#generate').click(function() {
     counter++;
     let newDie = new Die;
-    newDie;
     dice.push(newDie);
-    console.log(dice);
 });
 
 $('#roll').click(function() {
-    dice.forEach((i) => i.roll());
+    dice.forEach(function(i) {
+        i.value = i.roll();
+    });
+    console.log(dice);
 });
 
-// $('#sum').click(function(
+$('#sum').click(function() {
+    $('#sumbox').empty();
+    let valueArr = dice.map(i => i.value);
+    let sum = valueArr.reduce((a, b) => a + b);
+    $('#sumbox').text(sum);
+});
